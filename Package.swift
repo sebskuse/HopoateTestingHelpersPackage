3542c6dcc854058f820aa2ea5c38115c89c70a36
Stephen Anthony <steve@darjeelingapps.com>
2021-11-15 19:26:55 +0000

    Add MockContainer

--- Package.swift
+++ Package.swift
@@ -11,19 +11,14 @@ let package = Package(
         .watchOS(.v5)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "HopoateTestingHelpersPackage",
-            targets: ["HopoateTestingHelpersPackage"]),
+        .library(name: "HopoateTestingHelpersPackage", type: .dynamic, targets: ["HopoateTestingHelpersPackage"])
     ],
     dependencies: [
         .package(name: "Hopoate", url: "https://github.com/darjeelingsteve/hopoate", from: "1.7.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "HopoateTestingHelpersPackage",
-            dependencies: []),
+            dependencies: [.product(name: "Hopoate", package: "Hopoate")])
     ]
 )
