e28c8bc47413a76f3e35b512c33ed6260d6eb8f8
Stephen Anthony <steve@darjeelingapps.com>
2021-11-15 19:26:34 +0000

    Add Hopoate dependency

--- Package.swift
+++ Package.swift
@@ -17,8 +17,7 @@ let package = Package(
             targets: ["HopoateTestingHelpersPackage"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "Hopoate", url: "https://github.com/darjeelingsteve/hopoate", from: "1.7.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
